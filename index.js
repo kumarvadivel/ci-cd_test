daf6ae5241052120fd41e330860a91beb9b2a95f
vadivel539 <vadivel@codingmart.com>
2020-12-08 22:28:06 +0530

    fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express= require('express');
 const app = express();
-  
+const port = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
     res.send("welcome to home")
 })
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('listening on 3000')
 });
 
